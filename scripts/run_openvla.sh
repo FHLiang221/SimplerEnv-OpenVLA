5dee7048e41da3d5ae6681e29ea63e749aa22867
Your Name <your.email@example.com>
2025-09-18 13:54:24 -0700

    update readme

--- scripts/run_openvla.sh
+++ scripts/run_openvla.sh
@@ -13,8 +13,8 @@ tasks=(
   run_openvla_jaco.sh
 )
 ckpts=(
-  # fhliang/base_jaco_20_3k
-  fhliang/adv_jaco_20_3k
+  fhliang/jaco_base_20_10k
+  # fhliang/adv_jaco_20_3k
 )
 action_ensemble_temp=-0.8
 for ckpt_path in ${ckpts[@]}; do
