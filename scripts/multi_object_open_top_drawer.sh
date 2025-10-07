02f84cb80d341883960a57efbf5c7e4053bb939e
Freddie <freddie@example.com>
2025-10-07 15:35:19 -0700

    update episodes per task eval to 50

--- scripts/multi_object_open_top_drawer.sh
+++ scripts/multi_object_open_top_drawer.sh
@@ -27,6 +27,7 @@ do CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py \
   --use-proprio \
   --robot google_robot_static \
   --control-freq 3 --sim-freq 513 --max-episode-steps 150 \
+  --obj-episode-range 0 50 \
   --env-name ${task_name};
 
 done
